ed8b6bd2f0463375365c551b26d2ea77169ce816
Ronaldo Anjos <40528885+R0nald0@users.noreply.github.com>
2022-10-12 13:24:25 -0300

    Alterando binding para binding by lazy

--- Receitas/app/src/main/java/com/example/receitas/MainActivity.kt
+++ Receitas/app/src/main/java/com/example/receitas/MainActivity.kt
@@ -9,11 +9,11 @@ import com.example.receitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var rcView: RecyclerView
-    private lateinit var binding: ActivityMainBinding
-
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
      /*   rcView =findViewById(R.id.idRcView)
